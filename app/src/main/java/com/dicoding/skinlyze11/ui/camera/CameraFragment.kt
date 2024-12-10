20974164169414ef7752119c1cf0b48477b5c061
Muhammad Fauzan Naufal Ridho <101383801+ozannaufal15@users.noreply.github.com>
2024-12-10 11:31:59 +0800

    Update CameraFragment.kt

--- app/src/main/java/com/dicoding/skinlyze11/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/dicoding/skinlyze11/ui/camera/CameraFragment.kt
@@ -9,6 +9,16 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.skinlyze11.databinding.FragmentCameraBinding
 
+import android.content.ContentResolver
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Environment
+import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 class CameraFragment : Fragment() {
 
     private var _binding: FragmentCameraBinding? = null
@@ -38,5 +48,56 @@ class CameraFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    fun createCustomTempFile(context: Context): File {
+        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(timeStamp, ".jpg", storageDir)
+    }
+
+        @SuppressLint("QueryPermissionsNeeded")
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(this@CameraFragment, "com.example.foodhunt.camera", it)
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private var getFile: File? = null
+
+    private lateinit var currentPhotoPath: String
+    private val launcherIntentCamera = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            getFile = myFile
+
+            val result = BitmapFactory.decodeFile(getFile?.path)
+
+            binding.View.setImageBitmap(result)
+        }
+    }
+    fun sendMessage(view: View){
+        Toast.makeText(this, "Masuk ke Gallery", Toast.LENGTH_SHORT).show()
+    }
+
+    fun sendMessage2(view: View){
+        Toast.makeText(this, "Masuk ke Camera", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS=10
     }
 }
