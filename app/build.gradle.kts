7a646cb1cfe550a362aea87dd8f2df95f74ca75f
Muhammad Fauzan Naufal Ridho <101383801+ozannaufal15@users.noreply.github.com>
2024-12-10 11:19:28 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,22 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    implementation("androidx.fragment:fragment-ktx:1.5.7")
+
+    implementation("androidx.room:room-paging:2.6.0-alpha01")
+
+    implementation("androidx.room:room-runtime:2.5.1")
+    annotationProcessor("androidx.room:room-compiler:2.5.1")
+
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    implementation("com.github.bumptech.glide:glide:4.13.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+    implementation("com.google.android.material:material:1.4.0")
 }
